b342fe3e4449a884c32d0ce487017f363d14de9c
m.2@sandbox <m.2@sandbox>
2022-07-05 12:31:36 +0800

    set sample

---
+++ src/hooks/useApiMallOrder/index.js
@@ -0,0 +1,22 @@
+import useTokenRequest from 'zero-element-boot/lib/components/hooks/useTokenRequest'
+
+export default function useApiMallOrder({}){
+    const api = ''
+
+    /**
+     * 查询订单列表
+     * @param {} data 
+     */
+    function query(data){
+    }
+
+    function get({id, ...data}){
+    }
+
+    function set(data){
+    }
+
+    
+
+    return []
+}
